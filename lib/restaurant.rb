8c9ee48c2b883b9dde123ef84bbfee3df78de5e8
Austin Aspaas <araspaas@live.com>
2020-07-01 14:13:27 -0600

    Changed all single quotes to double quotes for concistancy in code

--- lib/restaurant.rb
+++ lib/restaurant.rb
@@ -37,7 +37,7 @@ class Restaurant
   def announce_closing_time(hours)
       time = closing_time(hours) + "AM"
     if closing_time(hours).to_i >= 12
-      time = (closing_time(hours).to_i - 12).to_s + ':00PM'
+      time = (closing_time(hours).to_i - 12).to_s + ":00PM"
     end
     "#{@name} will be closing at #{time}"
   end
--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -1,75 +1,75 @@
-require 'minitest/autorun'
-require 'minitest/pride'
-require './lib/restaurant'
-require 'pry'
+require "minitest/autorun"
+require "minitest/pride"
+require "./lib/restaurant"
+require "pry"
 
 class RestaurantTest < Minitest::Test
   # Iteration 1 Tests:
   def test_it_exists
-    restaurant = Restaurant.new('10:00', 'Fuel Cafe')
+    restaurant = Restaurant.new("10:00", "Fuel Cafe")
 
     assert_instance_of Restaurant, restaurant
   end
 
   def test_it_has_opening_time
-    restaurant = Restaurant.new('10:00', 'Fuel Cafe')
+    restaurant = Restaurant.new("10:00", "Fuel Cafe")
 
-    assert_equal '10:00', restaurant.opening_time
+    assert_equal "10:00", restaurant.opening_time
   end
 
   def test_it_has_name
-    restaurant = Restaurant.new('10:00', 'Fuel Cafe')
+    restaurant = Restaurant.new("10:00", "Fuel Cafe")
 
-    assert_equal 'Fuel Cafe', restaurant.name
+    assert_equal "Fuel Cafe", restaurant.name
   end
 
   def test_it_has_dishes
-    restaurant = Restaurant.new('10:00', 'Fuel Cafe')
+    restaurant = Restaurant.new("10:00", "Fuel Cafe")
 
     assert_equal [], restaurant.dishes
   end
 
   #Iteration 2 Tests:
   def test_it_has_closing_time
-    restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
-    restaurant2 = Restaurant.new('16:00', 'Il Poggio')
+    restaurant1 = Restaurant.new("10:00", "Fuel Cafe")
+    restaurant2 = Restaurant.new("16:00", "Il Poggio")
 
-    assert_equal '18:00', restaurant1.closing_time(8)
-    assert_equal '23:00', restaurant2.closing_time(7)
+    assert_equal "18:00", restaurant1.closing_time(8)
+    assert_equal "23:00", restaurant2.closing_time(7)
   end
 
   def test_it_can_add_dishes
-    restaurant = Restaurant.new('16:00', 'Il Poggio')
+    restaurant = Restaurant.new("16:00", "Il Poggio")
 
-    restaurant.add_dish('Burrata')
-    restaurant.add_dish('Pizzetta')
-    restaurant.add_dish('Ravioli')
+    restaurant.add_dish("Burrata")
+    restaurant.add_dish("Pizzetta")
+    restaurant.add_dish("Ravioli")
 
-    assert_equal ['Burrata', 'Pizzetta', 'Ravioli'], restaurant.dishes
+    assert_equal ["Burrata", "Pizzetta", "Ravioli"], restaurant.dishes
   end
 
   def test_it_is_open_for_lunch
-    restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
-    restaurant2 = Restaurant.new('16:00', 'Il Posto')
+    restaurant1 = Restaurant.new("10:00", "Fuel Cafe")
+    restaurant2 = Restaurant.new("16:00", "Il Posto")
 
     assert_equal true, restaurant1.open_for_lunch?
     assert_equal false, restaurant2.open_for_lunch?
   end
 
   def test_it_can_list_dishes_in_all_caps
-    restaurant1 = Restaurant.new('10:00', 'Fuel Cafe')
-    restaurant2 = Restaurant.new('16:00', 'Il Posto')
+    restaurant1 = Restaurant.new("10:00", "Fuel Cafe")
+    restaurant2 = Restaurant.new("16:00", "Il Posto")
 
-    restaurant2.add_dish('Burrata')
-    restaurant2.add_dish('Pizzetta')
-    restaurant2.add_dish('Ravioli')
+    restaurant2.add_dish("Burrata")
+    restaurant2.add_dish("Pizzetta")
+    restaurant2.add_dish("Ravioli")
 
     assert_equal ["BURRATA", "PIZZETTA", "RAVIOLI"], restaurant2.menu_dish_names
   end
 
   def test_it_can_announce_closing_time
-    restaurant1 = Restaurant.new('6:00', 'Fuel Cafe')
-    restaurant2 = Restaurant.new('16:00', 'Il Posto')
+    restaurant1 = Restaurant.new("6:00", "Fuel Cafe")
+    restaurant2 = Restaurant.new("16:00", "Il Posto")
 
     assert_equal "Fuel Cafe will be closing at 11:00AM", restaurant1.announce_closing_time(5)
     assert_equal "Il Posto will be closing at 11:00PM", restaurant2.announce_closing_time(7)
